d087724e134175a2acc7e73e89168f601c9beec8
knapo <knapo@knapo.net>
2013-06-14 11:29:08 +0200

    Add coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ require 'bundler/setup'
 
 require 'a9n'
 
+require 'coveralls'
+Coveralls.wear!
+
 RSpec.configure do |config|
   config.order = "random"
+  config.color_enabled = true
+  config.tty = true
 end
